5c69430f20959dcdd9620e844cb8a35e469a0ec9
Abd Salah <59089096+Abusalah0@users.noreply.github.com>
2025-01-03 10:47:23 +0300

    Update main.c

--- main.c
+++ main.c
@@ -27,3 +27,21 @@ int	main(int argc, char **argv)
 	mlx_loop(graphics.mlx);
 	return (0);
 }
+/*
+  _nnnn_                      
+        dGGGGMMb     ,"""""""""""""".
+       @p~qp~~qMb    | Linux Rules! |
+       M|@||@) M|   _;..............'
+       @,----.JM| -'
+      JS^\__/  qKL
+     dZP        qKRb
+    dZP          qKKb
+   fZP            SMMb
+   HZM            MMMM
+   FqM            MMMM
+ __| ".        |\dS"qML
+ |    `.       | `' \Zq
+_)      \.___.,|     .'
+\____   )MMMMMM|   .'
+     `-'       `--' hjm
+*/
